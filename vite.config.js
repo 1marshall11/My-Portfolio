064468996e336b8ca68159830b3e793cf36fb24a
1marshall11 <mayurrrathod21@gnu.ac.in>
2024-02-05 20:09:18 +0530

    2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
+  server:{
+    host: true,
+    port:80,
+  },
   plugins: [react()],
   base:"/My-Portfolio/",
   css:{
