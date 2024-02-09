43b4f4e3beb2d2dfe4cdcd7902670c4ca28cf116
1marshall11 <mayurrrathod21@gnu.ac.in>
2024-02-09 12:41:52 +0530

    2

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const [menuOpen,setMenuOpen] = useState(false);
   return (
   <nav className={styles.navbar}>
-    <a className={styles.title} href="/">Protfolio</a>
+    <a className={styles.title} href="/">Portfolio</a>
     <div className={styles.menu}>
       
     <img className={styles.menuBtn} src={menuOpen ? getImageUrl("nav/close-button.png")
